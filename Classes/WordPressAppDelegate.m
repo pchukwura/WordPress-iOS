c76019eac98b89539d0b55bc29f992051b363124
Danilo Ercoli <ercoli@gmail.com>
2012-12-05 11:51:52 +0100

    The liker_id parameter is included in the post liked notification.

--- Classes/WordPressAppDelegate.m
+++ Classes/WordPressAppDelegate.m
@@ -1148,6 +1148,7 @@
             /*
              [remoteNotif objectForKey:@"blog_id"]
              [remoteNotif objectForKey:@"post_id"]
+             [remoteNotif objectForKey:@"liker_id"]
              */
         } else if([nType isEqualToString:@"cl"]){
             //comment like
