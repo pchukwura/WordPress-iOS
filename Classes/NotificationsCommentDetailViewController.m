d00a08bfa8e4dc21a34833f24232492e5b7c88b8
Dan Roundhill <danroundhill@gmail.com>
2013-01-10 14:21:37 -0800

    Updated comment detail view to match design
    * Added grey background to parent comments
    * Parent comments have a smaller gravatar and lighter comment text
    * Fixed whacky animation that was showing when animating in the parent
    comment
    * Added some UIColor constants since we reuse link and background color
    quite often
    * Made the note-comment-grandparent-footer graphic a bit smaller so
    that it lines up with the center of the parent comment's gravatar

--- Classes/NotificationsCommentDetailViewController.m
+++ Classes/NotificationsCommentDetailViewController.m
@@ -158,7 +158,10 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
         [self.user getPath:postPath parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
             self.post = responseObject;
             self.disclosureIndicator.hidden = NO;
-            self.postBanner.titleLabel.text = [[self.post valueForKeyPath:@"title"] stringByDecodingXMLCharacters];
+            NSString *postTitle = [[self.post valueForKeyPath:@"title"] stringByDecodingXMLCharacters];
+            if (!postTitle || [postTitle isEqualToString:@""])
+                postTitle = NSLocalizedString(@"Unitled Post", @"Used when a post has no title");
+            self.postBanner.titleLabel.text = postTitle;
             id authorAvatarURL = [self.post valueForKeyPath:@"author.avatar_URL"];
             if ([authorAvatarURL isKindOfClass:[NSString class]]) {
                 [self.postBanner.avatarImageView setImageWithURL:[NSURL URLWithString:authorAvatarURL]];
@@ -265,10 +268,9 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
     frame.origin.y = self.tableView.contentSize.height;
     UIView *scrollBackView = [[UIView alloc] initWithFrame:frame];
     scrollBackView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
-    scrollBackView.backgroundColor = [UIColor greenColor];
     //[self.tableView addSubview:scrollBackView];
     self.tableView.backgroundView = [[UIView alloc] initWithFrame:self.tableView.bounds];
-    self.tableView.backgroundView.backgroundColor = [NoteCommentCell darkBackgroundColor];
+    self.tableView.backgroundView.backgroundColor = COMMENT_PARENT_BACKGROUND_COLOR;
 }
 
 
@@ -420,6 +422,7 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
             if (![parent isEqual:@0]) {
                 [self addScrollBackView];
                 parentComment = [[NoteComment alloc] initWithCommentID:[parent valueForKey:@"ID"]];
+                parentComment.isParentComment = YES;
             }
             
             CGPoint offset = self.tableView.contentOffset;
@@ -435,7 +438,7 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
                 // animate
                 [self.tableView beginUpdates];
                                 
-                [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:commentIndexPath.section] withRowAnimation:UITableViewRowAnimationNone];
+                //[self.tableView reloadSections:[NSIndexSet indexSetWithIndex:commentIndexPath.section] withRowAnimation:UITableViewRowAnimationNone];
 
                 if (parentComment) {
                     [self.commentThread insertObject:parentComment atIndex:0];
@@ -503,6 +506,8 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
                 if (headerCell == nil) {
                     headerCell = [[NoteCommentCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NotificationsCommentHeaderCellIdentifiter];
                 }
+                if ([comment isParentComment])
+                    [headerCell displayAsParentComment];
                 headerCell.delegate = self;
                 [self prepareCommentHeaderCell:headerCell forCommment:comment];
                 cell = headerCell;
@@ -528,10 +533,14 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
             }
             [self.contentCache setObject:contentCell forKey:comment];
             NSString *html = [comment.commentData valueForKey:@"content"];
+            if (!html)
+                html = self.note.commentText;
             if (html != nil) {
                 contentCell.attributedString = [self convertHTMLToAttributedString:html];
                 contentCell.selectionStyle = UITableViewCellSelectionStyleNone;
             }
+            if ([comment isParentComment])
+                [contentCell displayAsParentComment];
             cell = contentCell;
             break;
         }
@@ -580,13 +589,13 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
         return nil;
     } else {
         NSString *imageName;
-//        if (section == [self.commentThread count]-2) {
-//           // white
-//            imageName = @"note-comment-parent-footer";
-//        } else {
-//            imageName = @"note-comment-grandparent-footer";
-//        }
-        imageName = @"note-comment-parent-footer";
+        if (section == [self.commentThread count]-2) {
+           // white
+            imageName = @"note-comment-parent-footer";
+        } else {
+            imageName = @"note-comment-grandparent-footer";
+        }
+        //imageName = @"note-comment-parent-footer";
         UIEdgeInsets insets = UIEdgeInsetsMake(0.f, 68.f, 19.f, 0.f);
         UIImage *image = [[UIImage imageNamed:imageName] resizableImageWithCapInsets:insets];
         return [[UIImageView alloc] initWithImage:image];
@@ -607,7 +616,7 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
     CGFloat height;
     switch (indexPath.row) {
         case NotificationCommentCellTypeHeader:
-            height = (comment.isLoaded || mainComment) ? NoteCommentCellHeight : NoteCommentLoadingCellHeight;
+            height = (comment.isLoaded || mainComment) ? (comment.isParentComment) ? NoteCommentCellHeight - 36.0f : NoteCommentCellHeight : NoteCommentLoadingCellHeight;
             break;
         case NotificationCommentCellTypeContent:
         {
